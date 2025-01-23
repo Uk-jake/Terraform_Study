7ca7a48bbd9ea0f0d1a8eb9b9e2b24bfa13186ef
Uk-jake <mag0225@naver.com>
2025-01-23 12:11:56 +0900

    fix: change key pair

--- three_tier_architecture/modules/ec2/main.tf
+++ three_tier_architecture/modules/ec2/main.tf
@@ -11,3 +11,48 @@ resource "aws_instance" "ec2" {
         Name = "bastion_host"
     }   
 }
+
+# nat instance 생성
+resource "aws_instance" "nat_instance"{
+    ami = "ami-01c05ba56d996a2cf"
+    instance_type = "t2.micro"
+    subnet_id = var.public_subnet_ids[1]
+    vpc_security_group_ids = [var.security_group_id]
+    availability_zone = "ap-northeast-2c"
+    key_name = var.key_name
+
+    # nat instance는 source/destination check를 비활성화해야 함
+    source_dest_check = false
+
+    tags = {
+        Name = "nat_instance"
+    }
+}
+
+# web1 instance 생성
+resource "aws_instance" "web1" {
+    ami = "ami-040c33c6a51fd5d96"
+    instance_type = "t2.micro"
+    subnet_id = var.private_subnet_ids[0]
+    vpc_security_group_ids = [var.security_group_id]
+    availability_zone = "ap-northeast-2a"
+    key_name = var.key_name
+
+    tags = {
+        Name = "web1"
+    }
+}
+
+# web2 instance 생성
+resource "aws_instance" "web2" {
+    ami = "ami-040c33c6a51fd5d96"
+    instance_type = "t2.micro"
+    subnet_id = var.private_subnet_ids[1]
+    vpc_security_group_ids = [var.security_group_id]
+    availability_zone = "ap-northeast-2c"
+    key_name = var.key_name
+
+    tags = {
+        Name = "web2"
+    }
+}
--- three_tier_architecture/modules/key_pair/main.tf
+++ three_tier_architecture/modules/key_pair/main.tf
@@ -9,7 +9,8 @@ resource "aws_key_pair" "my_key" {
     public_key = tls_private_key.example.public_key_openssh
 }
 
+# private key를 local file로 저장
 resource "local_file" "private_key" {
     content = tls_private_key.example.private_key_pem
-    filename = "{$path.module}/my_key.pem"
+    filename = "${path.module}/my_key.pem"  # 수정된 경로
 }
